911114e251362aac4e06f41322cc2f57cae648c7
Daniel Firebanks-Quevedo <dafirebanks@gmail.com>
2023-05-21 14:56:29 -0700

    moved folders around

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/layout.tsx": [
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/network/page": [
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/globals.css",
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/page": [
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/app/globals.css",
+      "/Users/dafirebanks/Projects/the-yin-score/the-yin-score/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
